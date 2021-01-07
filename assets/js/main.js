eceb8f50969ae131464c5f05d7a2cc2977fbd75e
TobiasBuse <buse.tobias@googlemail.com>
2021-01-07 17:13:12 +0100

    RestHamb

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+function changeFontColor(){
+    document.getElementById("body").style.color="#fff";
+    document.getElementById("body").style.background="#000";
+
+    console.log("it works")
+}
+
+
+function changeFontColor1(){
+    document.getElementById("body").style.color="#000";
+    document.getElementById("body").style.background="#fff";
+
+    console.log("it works")
+}
+
+
+function changeFontColor2(){
+    document.getElementById("body").style.color="#f4c4c4";
+    document.getElementById("body").style.background="#282b29";
+
+    console.log("it works")
+}
+
+// function removeFontColor(){
+//     document.getElementById("font-color").classList.remove("font-color")
+// }
+
+
+// function addFontColor2(){
+//     document.getElementById("font-color2").classList.add("font-color2")
+// }
+
+
+function showApplicationDetails(){
+    document.getElementById("text1").classList.toggle("hide")
+}
+
+
+function showEntryRequirements(){
+    document.getElementById("text2").classList.toggle("hide")
+}
